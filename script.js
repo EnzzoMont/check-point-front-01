6c43b32291abfccf256b09221a7c317203afa8ce
Helldeck <rm552616@fiap.com.bt>
2023-09-29 11:14:23 -0300

    front-web

---
+++ script.js
@@ -0,0 +1,41 @@
+const nome = "Enzzo Monteiro Barros Silva"
+const idade = 18
+const status = true
+const ValorProdute = 10.50
+const times = ["Corinthias", "São paulo", "Barcelona"]
+
+alert
+
+console.log(nome)
+console.log(idade)
+console.log(status)
+console.log(ValorProdute)
+console.log(times)
+
+// Soma
+function soma (valor1, valor2){
+    const resultado = valor1 + valor2
+    console.log(resultado)
+}
+soma(16, 11)
+
+// Subtração
+function subracao (valor1, valor2){
+    const resultado = valor1 - valor2
+    console.log(resultado)
+}
+subracao(16, 11)
+
+// Multiplicão
+function mult(valor1, valor2){
+    const resultado = valor1 * valor2
+    console.log(resultado)
+}
+mult(16, 11)
+
+// Divisão
+function divi(valor1, valor2){
+    const resultado = valor1 / valor2
+    console.log(resultado)
+}
+divi(16, 11)
